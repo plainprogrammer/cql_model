b07d7e6b4da44c5333908a58f617f179040c2b4f
James Thompson <james@plainprograms.com>
2013-06-22 23:34:03 -0600

    Refactored Cql::Model::FinderMethods spec.

--- spec/cql/model/finders_spec.rb
+++ spec/cql/model/finders_spec.rb
@@ -1,36 +1,29 @@
 require 'spec_helper'
 
-describe Cql::Model do
+describe 'Cql::Model Finders' do
   before :all do
     Cql::Base.establish_connection(host: '127.0.0.1')
     Cql::Base.connection.use('cql_model_test')
   end
 
-  describe 'Finders' do
-    describe '#all' do
-      it { Person.all.must_be_instance_of Cql::Model::QueryResult }
-      it { Person.all.first.must_be_instance_of Person }
-    end
+  describe '#all' do
+    it { Person.all.must_be_instance_of Cql::Model::QueryResult }
+    it { Person.all.first.must_be_instance_of Person }
+  end
 
-    describe '#find(1)' do
+  describe '#find' do
+    describe 'single records' do
       it { Person.find(1).must_be_instance_of Person }
-    end
-
-    describe "#find('1')" do
       it { Person.find('1').must_be_instance_of Person }
     end
 
-    describe '#find(1,2)' do
+    describe 'multiple/array records' do
       it { Person.find(1,2).must_be_instance_of Array }
       it { Person.find(1,2).size.must_equal 2 }
-    end
 
-    describe '#find([1])' do
       it { Person.find([1]).must_be_instance_of Array }
       it { Person.find([1]).size.must_equal 1 }
-    end
 
-    describe '#find([1,2])' do
       it { Person.find([1,2]).must_be_instance_of Array }
       it { Person.find([1,2]).size.must_equal 2 }
     end
